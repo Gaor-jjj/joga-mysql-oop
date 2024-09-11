db95e052f4e478e30830c715e9e8f50a4e828c6e
gaor-jjj <kaurtamm1995@gmail.com>
2024-09-11 13:23:22 +0300

    Create user login

--- controllers/user.js
+++ controllers/user.js
@@ -36,6 +36,29 @@ class userController {
             })
         }
     }
+
+    async login(req, res) {
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne(username);
+        if (!user) {
+            return res.status(400).json({ error: 'Ivalid username or password' })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ error: 'Invalid username or password' })
+        }
+
+        req.session.user = {
+            username: user.username,
+            user_id: user.id
+        }
+        res.json({
+            message: 'Login successful',
+            user_session: req.session.user
+        })
+    }
 }
 
 module.exports = userController
