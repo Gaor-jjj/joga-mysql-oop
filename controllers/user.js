53599571f53b1c5d1c867456bff82c76c451be5b
gaor-jjj <kaurtamm1995@gmail.com>
2024-09-11 11:30:59 +0300

    Fix existing user and password limitation errors

--- controllers/user.js
+++ controllers/user.js
@@ -5,10 +5,22 @@ const userModel = new userDbModel()
 class userController {
 
     async register(req, res) {
+
+        const { username, email, password } = req.body;
+
+        if (password.length < 6) {
+            return res.status(400).json({ error: 'Password must be at least 6 characters long' });
+        }
+
+        const existingUser = await userModel.findOne(username);
+        if (existingUser) {
+            return res.status(400).json({ error: 'Username already exists' });
+        }
+
         const cryptPassword = await bcrypt.hash(req.body.password, 10)
         const registeredId = await userModel.create({
-            username: req.body.username,
-            email: req.body.email,
+            username,
+            email,
             password: cryptPassword
         })
 
